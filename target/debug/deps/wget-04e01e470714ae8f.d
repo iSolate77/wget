7696fc79bd465f75ba2b006a1e47fdb177c98284
Mohamed Faris <mohd.faris07@gmail.com>
2023-09-05 17:52:58 +0300

    my own branch

---
+++ target/debug/deps/wget-04e01e470714ae8f.d
@@ -0,0 +1,5 @@
+/Users/mohdfaris/reboot/wget/target/debug/deps/wget-04e01e470714ae8f.rmeta: src/main.rs
+
+/Users/mohdfaris/reboot/wget/target/debug/deps/wget-04e01e470714ae8f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/wget-d0737082581b69d7.d
@@ -0,0 +1,5 @@
+/Users/mohdfaris/reboot/wget/target/debug/deps/wget-d0737082581b69d7.rmeta: src/main.rs
+
+/Users/mohdfaris/reboot/wget/target/debug/deps/wget-d0737082581b69d7.d: src/main.rs
+
+src/main.rs:
